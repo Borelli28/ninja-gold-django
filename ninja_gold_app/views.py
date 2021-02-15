ff0b6e4ffa132b96c98e7797fc00e0b7e41aba85
armando <borelliarmando12@gmail.com>
2021-02-15 15:08:01 -0600

    Figure out acitvity logs but now farm method is broken dont know why

--- ninja_gold_app/views.py
+++ ninja_gold_app/views.py
@@ -3,6 +3,7 @@ import random
 
 # python manage.py
 
+
 def index(request):
 
     if 'gold_count' in request.session:
@@ -13,33 +14,37 @@ def index(request):
     if 'activity' in request.session:
         print("logs!")
     else:
-        request.session['activity'] = "ACTIVITY LOG"
+        request.session['activity'] = []
+        # request.session.save()
 
     return render(request, 'index.html')
 
 
-def process_farm(request):
+# def process_farm(request):
 
-    # FARM earn 10 - 20 gold
-    random_gold = random.randint(10, 20)
-    request.session['gold_count'] += random_gold
-    print(request.session['gold_count'])
+#     # FARM earn 10 - 20 gold
+#     random_gold = random.randint(10, 20)
+#     request.session['gold_count'] += random_gold
+#     print(request.session['gold_count']
+#     # logs.append(f"{random_gold} gold added to count")
 
-    return redirect('/')
+#     return redirect('/')
 
 def process_cave(request):
 
     # CAVE earn 5 - 10 gold
-    random_gold = random.randint(5, 10)
+    random_gold=random.randint(5, 10)
     request.session['gold_count'] += random_gold
     print(request.session['gold_count'])
+    request.session['activity'].append(f"{random_gold} gold added to count")
+    request.session.save()
 
     return redirect('/')
 
 def process_house(request):
 
     # HOUSE earn 2 - 5 gold
-    random_gold = random.randint(2, 5)
+    random_gold=random.randint(2, 5)
     request.session['gold_count'] += random_gold
     print(request.session['gold_count'])
 
@@ -48,7 +53,7 @@ def process_house(request):
 def process_casino(request):
 
     # CASINO Earn/Lose 0 - 50 gold
-    random_gold = random.randint(-50, 50)
+    random_gold=random.randint(-50, 50)
     print(random_gold)
     # No need to check for negatives because( 10 + -5 = 5)
     request.session['gold_count'] += random_gold
@@ -62,4 +67,3 @@ def clear(request):
     print("Session was cleared")
 
     return redirect('/')
-
